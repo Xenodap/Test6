44da6bc798c5f016c6e844aee45d470455f4a7fa
Song <scw387@naver.com>
2023-07-19 17:53:32 +0900

    모바일 회원가입 로그인 만들기 틀

---
+++ test6_scw/src/main/java/com/example/test6_scw/IntroActivity.kt
@@ -0,0 +1,24 @@
+package com.example.test6_scw
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+
+//        val handler:Handler=Handler();
+        Handler().postDelayed({
+                val intent=Intent(this@IntroActivity,MainActivity::class.java)
+                startActivity(intent)
+            finish()
+        },3000)
+
+    }
+
+}
